96b4eb931a7a93b4034a79faf9bbe8d52e659f05
Marwan <kaka.mer1998@gmail.com>
2023-02-02 15:15:57 +0300

    feat: Add a pre-loader while the page is loading

--- madlibs.js
+++ madlibs.js
@@ -204,3 +204,27 @@ madLibsEdit.forEach(function (element) {
     firstInput.focus();
   });
 });
+
+// pre-loader function
+var loader;
+
+function loadNow(opacity) {
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
+      loadNow(opacity - 0.05);
+    }, 50);
+  }
+}
+
+function displayContent() {
+  loader.style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  loader = document.getElementById("loader");
+  loadNow(1);
+});
